8a1be01e419b32dec07bdb5c8faf2c7d310d4c5a
Cheng Zhao <chengzhao41@gmail.com>
2014-06-29 16:17:24 -0400

    Fixed robots.txt

--- picasso/picasso/urls.py
+++ picasso/picasso/urls.py
@@ -33,7 +33,7 @@ urlpatterns = patterns('',
                                                   mimetype="text/plain")),
                        url(r'BingSiteAuth\.xml', TemplateView.as_view(template_name='BingSiteAuth.xml')),
                        (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-                       (r'^robots\.txt$', TemplateView.as_view, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+                       (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', mimetype='text/plain')),
 
                        # Examples:
                        # url(r'^$', 'picasso.views.home', name='home'),
