7be5d1b9fdac08a9972a8e6d836eaa2d69c33402
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-03 18:03:07 -0400

    add all assosiations

--- picasso/karate_ontario.py
+++ picasso/karate_ontario.py
@@ -11,54 +11,54 @@ from picasso.index.models import Listing, Tag, Address
 __author__ = 'tmehta'
 base_url = 'https://members.karate-ontario.com/members/memberships.html/assoc-page/map-mapper?association_id='
 
-tag = Tag.objects.get_or_create(tag_name='Karate', dash_version='karate')
-i = 642
-scraped_url = base_url + str(i)
-try:
-    Listing.objects.get(scraped_url=scraped_url)
-except Listing.DoesNotExist:
-    r = requests.get(scraped_url)
-    if "Association is inactive" not in r.text:
-        soup = BeautifulSoup(r.text)
-        content = soup.select('.rightbox_content')[0].findAll(text=True)
-        content = [re.sub(' +', ' ', x.replace('\n', '').strip()) for x in content if x != '\n']
-        if len(content) == 15:
-            print content
-            listing_name = content[0]
-            school = content[1]
-            site = content[4]
-            email = content[7]
-            location = content[9]
-            city = content[10].split(',')[0]
-            country = 'Canada'
-            postal_code = content[11]
-            phone = content[13].replace('Tel: ', '')
-            try:
-                results = Geocoder.geocode(str(location + ' ' + postal_code + ' Canada'))
-                lat, lon = results[0].coordinates
-            except IndexError:
-                lat, lon = 43.7, 79.4
-            except GeocoderError:
-                lat, lon = 43.7, 79.4
-            print lat, lon
-            l = Listing.objects.create(listing_name=listing_name, scraped_url=scraped_url)
-            l.description = ''
-            l.email = email
-            l.phone = phone
-            l.active = True
-            if l.address is None:
+tag = Tag.objects.get(tag_name='Karate', dash_version='karate')
+for i in range(1, 1000):
+    scraped_url = base_url + str(i)
+    try:
+        Listing.objects.get(scraped_url=scraped_url)
+    except Listing.DoesNotExist:
+        r = requests.get(scraped_url)
+        if "Association is inactive" not in r.text:
+            soup = BeautifulSoup(r.text)
+            content = soup.select('.rightbox_content')[0].findAll(text=True)
+            content = [re.sub(' +', ' ', x.replace('\n', '').strip()) for x in content if x != '\n']
+            if len(content) == 15:
+                print content
+                listing_name = content[0]
+                school = content[1]
+                site = content[4]
+                email = content[7]
+                location = content[9]
+                city = content[10].split(',')[0]
+                country = 'Canada'
+                postal_code = content[11]
+                phone = content[13].replace('Tel: ', '')
                 try:
+                    results = Geocoder.geocode(str(location + ' ' + postal_code + ' Canada'))
+                    lat, lon = results[0].coordinates
+                except IndexError:
+                    lat, lon = 43.7, 79.4
+                except GeocoderError:
+                    lat, lon = 43.7, 79.4
+                print lat, lon
+                l = Listing.objects.create(listing_name=listing_name, scraped_url=scraped_url)
+                l.description = ''
+                l.email = email
+                l.phone = phone
+                l.active = True
+                if l.address is None:
+                    try:
+                        point = "POINT(%s %s)" % (lon, lat)
+                        add = Address.objects.create(city=city, location=location, postal_code=postal_code,
+                                                     point=geos.fromstr(point))
+                        l.address = add
+                    except Exception as e:
+                        print postal_code
+                else:
                     point = "POINT(%s %s)" % (lon, lat)
-                    add = Address.objects.create(city=city, location=location, postal_code=postal_code,
-                                                 point=geos.fromstr(point))
-                    l.address = add
-                except Exception as e:
-                    print postal_code
+                    l.address.point = point
+                    l.address.save()
+                l.tags = [tag.id]
+                l.save()
             else:
-                point = "POINT(%s %s)" % (lon, lat)
-                l.address.point = point
-                l.address.save()
-            l.tags = [tag.id]
-            l.save()
-        else:
-            print content
+                print content
