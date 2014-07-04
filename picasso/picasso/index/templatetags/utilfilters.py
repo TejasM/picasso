b0803343a5859d503e6755c5e56bfd227f7a4ed0
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-03 20:00:47 -0400

    Add filter

---
+++ picasso/picasso/index/templatetags/utilfilters.py
@@ -0,0 +1,22 @@
+__author__ = 'tmehta'
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter('format_distance')
+def format_distance_(distance):
+    distance = distance.strip()
+    l = []
+    for t in distance.split():
+        try:
+            l.append(float(t))
+        except ValueError:
+            pass
+
+    l = l[0]
+    l = int(l / 100) / 10
+    string = "{0:.2f}".format(round(l, 2))
+    string += " km"
+    return string
