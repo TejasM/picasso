b79c9d5598a41f1e00708c729cec4f406f91c2a5
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-08 13:38:31 -0400

    Only ones with something

--- picasso/picasso/context_processor.py
+++ picasso/picasso/context_processor.py
@@ -1,9 +1,10 @@
+from django.db.models import Q
 from picasso.index.models import Tag
 
 __author__ = 'tmehta'
 
 
 def get_current_tags(request):
-    full_tags = Tag.objects.all().order_by('?')
+    full_tags = Tag.objects.filter(~Q(listing=None)).order_by('?')
     tags = [str(x) for x in full_tags.values_list('tag_name', flat=True)]
     return {'tags': tags, 'full_tags': full_tags}
