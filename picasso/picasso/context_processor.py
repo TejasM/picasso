762d24088ea207e2ba7e8c73fa27128268db0866
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-16 12:02:19 -0400

    only main tag

--- picasso/picasso/context_processor.py
+++ picasso/picasso/context_processor.py
@@ -5,6 +5,6 @@ __author__ = 'tmehta'
 
 
 def get_current_tags(request):
-    full_tags = Tag.objects.filter(~Q(listing=None)).order_by('?')
+    full_tags = Tag.objects.filter(parent_tag=None).order_by('?')
     tags = [str(x) for x in full_tags.values_list('tag_name', flat=True)]
     return {'tags': tags, 'full_tags': full_tags}
