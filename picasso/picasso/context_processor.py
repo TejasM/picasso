73ac00e342083b72282e931bf44478a27095402e
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-18 11:00:36 -0400

    fix

--- picasso/picasso/context_processor.py
+++ picasso/picasso/context_processor.py
@@ -6,9 +6,9 @@ __author__ = 'tmehta'
 
 def get_current_tags(request):
     full_tags = Tag.objects.annotate(num_listings=Count('listings')).filter(parent_tag=None, visible=True).order_by(
-        'num_listings')
-    all_tags = Tag.objects.annotate(num_listings=Count('listings')).filter(visible=True).order_by('num_listings')
-    all_tags = [str(x.tag_name) for x in all_tags]
+        '-num_listings')
+    all_tags = Tag.objects.annotate(num_listings=Count('listings')).filter(visible=True).order_by('-num_listings')
+    all_tags = [str(x.tag_name) for x in all_tags if x.tag_name != 'Blank' and x.tag_name != '']
     count = Listing.objects.count()
-    tags = [str(x.tag_name) for x in full_tags]
+    tags = [str(x.tag_name) for x in full_tags if x.tag_name != 'Blank' and x.tag_name != '']
     return {'tags': tags, 'full_tags': full_tags, 'all_tags': all_tags, 'number_of_listings': count}
