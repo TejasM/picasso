23c03c18e40d8a6e08b064b2b18bf2904d44a605
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-21 09:02:52 -0400

    Add city

--- picasso/picasso/index/models.py
+++ picasso/picasso/index/models.py
@@ -83,16 +83,15 @@ class Address(BaseModel):
         return string
 
     def save(self, **kwargs):
-        if self.point is None:
-            try:
-                results = Geocoder.geocode(
-                    str(self.location + ' ' + self.state + ' ' + self.country + ' ' + self.postal_code))
-                lat, lon = results[0].coordinates
-            except IndexError:
-                lat, lon = 43.7, 79.4
-            except GeocoderError:
-                lat, lon = 43.7, 79.4
-            self.point = "POINT(%s %s)" % (lon, lat)
+        try:
+            results = Geocoder.geocode(
+                str(self.location + ' ' + self.state + ' ' + self.city + ' ' + self.country + ' ' + self.postal_code))
+            lat, lon = results[0].coordinates
+        except IndexError:
+            lat, lon = 43.7, 79.4
+        except GeocoderError:
+            lat, lon = 43.7, 79.4
+        self.point = "POINT(%s %s)" % (lon, lat)
         super(Address, self).save(**kwargs)
 
 
