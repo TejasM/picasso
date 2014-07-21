aeb8af64dfb5eb9bbf8df6e0f6322ebe526a9538
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-21 19:56:56 -0400

    Add decoder

--- picasso/picasso/utils.py
+++ picasso/picasso/utils.py
@@ -23,3 +23,25 @@ def send_claim_email(listing):
         msg.send()
         return "Success"
     return "Fail"
+
+
+def decodeHtmlentities(string):
+    import re
+
+    entity_re = re.compile("&(#?)(\d{1,5}|\w{1,8});")
+
+    def substitute_entity(match):
+        from htmlentitydefs import name2codepoint as n2cp
+
+        ent = match.group(2)
+        if match.group(1) == "#":
+            return unichr(int(ent))
+        else:
+            cp = n2cp.get(ent)
+
+            if cp:
+                return unichr(cp)
+            else:
+                return match.group()
+
+    return entity_re.subn(substitute_entity, string)[0]
